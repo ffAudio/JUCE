a2ebd2806ea1844a340773c71ba34bc1e6bbeca1
Daniel Walz <developer@foleysfinest.com>
2017-09-27 01:01:15 +0200

    Updated JUCE docs for develop

--- search/all_14.js
+++ search/all_14.js
@@ -7,7 +7,7 @@ var searchData=
   ['undomanager',['undomanager',['../group__juce__data__structures-undomanager.html',1,'']]],
   ['utils',['utils',['../group__juce__opengl-utils.html',1,'']]],
   ['uicolour',['UIColour',['../classjuce_1_1LookAndFeel__V4_1_1ColourScheme.html#a9fd1cab41612581942d93497a954ab71',1,'juce::LookAndFeel_V4::ColourScheme']]],
-  ['uid',['UID',['../classjuce_1_1Block.html#a0083bcd745755b891f0e53e8a32633f3',1,'juce::Block::UID()'],['../classjuce_1_1PluginDescription.html#ab3895f046dd542048a963ff5f471a7d7',1,'juce::PluginDescription::uid()'],['../classjuce_1_1Block.html#a05952b9650107a6ff52cb41453184525',1,'juce::Block::uid()']]],
+  ['uid',['uid',['../classjuce_1_1PluginDescription.html#ab3895f046dd542048a963ff5f471a7d7',1,'juce::PluginDescription::uid()'],['../classjuce_1_1Block.html#a05952b9650107a6ff52cb41453184525',1,'juce::Block::uid()'],['../classjuce_1_1Block.html#a0083bcd745755b891f0e53e8a32633f3',1,'juce::Block::UID()']]],
   ['uint16',['uint16',['../group__juce__blocks__basics-littlefoot.html#gaab688ac828c9bbf4cfc80f684f2d522b',1,'littlefoot::uint16()'],['../group__juce__core-maths.html#ga05bed2f5209971cef4ea323aabb5b55e',1,'juce::uint16()']]],
   ['uint32',['uint32',['../group__juce__blocks__basics-littlefoot.html#ga417dab5137c6d4ff4e23303f84099aa9',1,'littlefoot::uint32()'],['../group__juce__core-maths.html#ga9c13f39daaa50b986489e39f22b8a9ee',1,'juce::uint32()']]],
   ['uint64',['uint64',['../group__juce__core-maths.html#gaae917ec7c857bdbcbf65c5ae0df3697d',1,'juce']]],
