c51339c23b222f074ba4b6ebba84dcec20bb3a57
Daniel Walz <developer@foleysfinest.com>
2019-06-18 01:00:56 +0200

    Updated JUCE docs for develop

--- search/typedefs_12.js
+++ search/typedefs_12.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['value',['value',['../group__juce__audio__plugin__client-Unity.html#ga7d209388070aba8c66e5b8bc12ff1725',1,'juce_UnityPluginInterface.h']]],
   ['value_5ftype',['value_type',['../structValueTree_1_1Iterator.html#ac54f6a5cdd9dcc2345eee2f8f9be086c',1,'ValueTree::Iterator::value_type()'],['../structdsp_1_1SIMDRegister.html#ac38979ca94af0b812cdf1f9206a8b19c',1,'dsp::SIMDRegister::value_type()']]],
+  ['valueremapfunction',['ValueRemapFunction',['../classNormalisableRange.html#a413315ca8a7cb8b6d44e01f0d28bde13',1,'NormalisableRange']]],
   ['valuestr',['valuestr',['../group__juce__audio__plugin__client-Unity.html#ga39b187949529e2a44358c41308978552',1,'juce_UnityPluginInterface.h']]],
   ['vectortype',['VectorType',['../classDraggable3DOrientation.html#aa93e8f8ea935cf7d06c99c27e745c5e3',1,'Draggable3DOrientation']]],
   ['versionnumber',['VersionNumber',['../namespaceBlocksProtocol.html#ac29fc2bccf0e3f21100a3ec714fcff1e',1,'BlocksProtocol']]],
